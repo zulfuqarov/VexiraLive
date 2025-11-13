ad466fe3957ca3534380f199c813d0caf054c0e4
Nebi Zulfuqarov <nebimac.zulfuqarovmac@gmail.com>
2025-11-13 11:56:15 +0400

    update

--- src/api/Requests/Video/Video.ts
+++ src/api/Requests/Video/Video.ts
@@ -4,14 +4,14 @@ import { VideoCategoryType, VideoType } from '../../../type/ApiType/VideoType';
 
 export const GetVideoCategories = async (): Promise<VideoCategoryType[]> => {
   const response = await api.get<VideoCategoryType[]>(
-    `player_api.php?username=${process.env.API_USERNAME}a&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_Categories}`,
+    `player_api.php?username=${process.env.API_USERNAME}&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_Categories}`,
   );
   return response.data;
 };
 
 export const GetAllVideos = async (): Promise<VideoType[]> => {
   const response = await api.get<VideoType[]>(
-    `player_api.php?username=${process.env.API_USERNAME}a&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_All}`,
+    `player_api.php?username=${process.env.API_USERNAME}&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_All}`,
   );
   return response.data;
 };
@@ -20,7 +20,7 @@ export const GetVideosByCategoryId = async (
   categoryId: string,
 ): Promise<VideoType[]> => {
   const response = await api.get<VideoType[]>(
-    `player_api.php?username=${process.env.API_USERNAME}a&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_Category_ById}${categoryId}`,
+    `player_api.php?username=${process.env.API_USERNAME}&password=${process.env.API_PASSWORD}&action=${APIEndPoints.Api_Category_ById}${categoryId}`,
   );
   return response.data;
 };
